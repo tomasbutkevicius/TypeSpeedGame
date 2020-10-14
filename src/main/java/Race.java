1ea05e838284b6c742c5627f3b36473792c5aa71
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2020-10-14 15:35:43 +0300

    typed words can be compared with given

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Race {
     public static Boolean start(ArrayList<Player> players){
-
         Player player = PlayerController.createPlayer(players);
 
         if(player == null){
@@ -19,28 +18,47 @@ public class Race {
 
         String sentence;
         Integer totalWords = 0;
-        Long seconds = 0L;
         int repeat = 4;
+        ArrayList<String> givenWords = new ArrayList<>();
 
         SentenceGenerator sentenceGenerator = new SentenceGenerator();
         while (repeat != 0) {
-            String[] words;
             sentence = sentenceGenerator.getSentence();
             System.out.print(sentence.substring(0, 1).toUpperCase() + sentence.substring(1));
-            words = sentence.split("\\s");
+
+            String[] words = sentence.split("\\s");
             totalWords += words.length;
+            for(String word: words){
+                givenWords.add(word);
+            }
+
             repeat = repeat - 1;
         }
 
         System.out.println();
+
+
         System.out.println("Repeat:");
         TimeWatch watch = TimeWatch.start();
-        String typedWords = scanner.nextLine();
-        seconds = watch.time(TimeUnit.SECONDS);
-        System.out.println("total words: " + totalWords);
+        String input = scanner.nextLine();
+        Long seconds = watch.time(TimeUnit.SECONDS);
+        System.out.println(seconds);
+        String[] typedWords = input.split("\\s");
+
+        System.out.println("given WORDS:");
+        for(int i = 0; i < givenWords.size(); i++){
+            System.out.print(givenWords.get(i) + " ");
+        }
+        System.out.println("typed WORDS:");
+        for(int i = 0; i < typedWords.length; i++){
+            System.out.print(typedWords[i] + " ");
+        }
+
         Long wordsPerMinute = (totalWords * 60) / seconds;
         player.setWordsPerMinute(wordsPerMinute);
+
         Printer.printPlayerList(players, player);
+
         System.out.println("Enter any key to continue...");
         scanner.nextLine();
         return true;
